9721d47fa80f394c4e68a27119920e638796e8ce
Charles Zheng <snarles@stanford.edu>
2014-04-17 13:38:56 +0000

    changed to odd, multistep

--- inftree.R
+++ inftree.R
@@ -1,16 +1,19 @@
 x = (1:1000)/1000
 
-plotk = function(k) {
-  y1 = x^k
-  y = 1-(1-y1)^k
-  plot(x,y,ylim=c(-1,1),type="l")
+plotkn = function(k,n=1,x=x) {
+  y1=x
+  for (i in 1:n) {
+    y = 1-(1-y1)^k    
+    y1 = y^k
+  }
+  plot(x,y,type="l")
   lines(x,y1,col="blue")
   lines(x,x,col="red")
   abline(0,0)
-  ind=which(c(y-x,0) > 0 & c(0,y-x) < 0)
-  crit = (-(y-x)[ind-1]*x[ind-1] + (y-x)[ind]*x[ind])/((y-x)[ind]-(y-x)[ind-1])
-  lines(c(crit,crit),c(-1,1))
-  title(paste("k=",k),sub=crit)
+  #ind=which(c(y-x,0) > 0 & c(0,y-x) < 0)
+  #crit = (-(y-x)[ind-1]*x[ind-1] + (y-x)[ind]*x[ind])/((y-x)[ind]-(y-x)[ind-1])
+  #lines(c(crit,crit),c(-1,1))
+  #title(paste("k=",k," n=",n),sub=crit)
 }
 
 plotk2 = function(p,q,k) {
