cfa429edf17dc7356ea1b9a8f79c4f85999b619d
Charles Zheng <snarles@stanford.edu>
2015-03-25 17:20:13 -0700

    empirical vs theoretical

--- ident_regression/toy2.R
+++ ident_regression/toy2.R
@@ -2,9 +2,6 @@
 ## Theory
 ############
 
-bt <- 2
-sigma2_x <- 0.8
-sigma2_eps <- 0.5
 
 ## empirical misclassification rate
 simulate <- function(al, k_cl, n_trials, seed = 0) {
@@ -32,7 +29,7 @@ p_folded <- function(mu, sigma2, thres, empirical = FALSE) {
 }
 
 ## misclassification rate based on conditional dist
-theory1 <- function(al, k_cl, res = 10) {
+theory1 <- function(al, k_cl, res = 30) {
   # normal distribution grid
   g0 <- -res:res
   z2 <- cbind(rep(g0, 2*res + 1),
@@ -48,6 +45,44 @@ theory1 <- function(al, k_cl, res = 10) {
   ps <- p_folded(mus, sigma2, thress)
   1 - sum(ps^(k_cl-1) * d2)
 }
+
+## quantity derived from theory 1
+theory2 <- function(al, res = 30) {
+  # normal distribution grid
+  g0 <- -res:res
+  z2 <- cbind(rep(g0, 2*res + 1),
+              rep(g0, each = 2*res + 1)) /
+    sqrt(res)
+  d2 <- exp(-.5 * z2[,1]^2 - .5 * z2[,2]^2 )
+  d2 <- d2/sum(d2)
+  xs <- sqrt(sigma2_x) * z2[,1]
+  eps <- sqrt(sigma2_eps) * z2[,2]
+  mus <- bt * xs + eps
+  sigma2 <- al^2 * sigma2_x
+  thress <- (bt - al) * xs + eps
+  ps1 <- 1 + sign(thress) * pnorm(xs / sqrt(sigma2_x))
+  ps2 <- sign(thress) * 
+     pnorm(((-2*bt + al)*xs - 2*eps)/(al * sqrt(sigma2_x)))
+  ps <- ps1 + ps2
+  c(1 - sum(ps * d2), sum(ps1 * d2), sum(ps2 * d2))
+}
+
+
+
 library(class)
-mean(simulate(2, 3, 1000))
-theory1(2, 3, 30)
+
+bt <- 100
+sigma2_x <- 1
+sigma2_eps <- 1
+#mean(simulate(2, 3, 1000))
+k_cl <- 10
+sapply(bt + .001 * 0:10, function(x) {theory1(x, k_cl)})
+sapply(bt - .001 * 0:10, function(x) {theory1(x, k_cl)})
+
+theory1(bt, 100)
+theory1(bt + .01, 100)
+theory1(bt - .01, 100)
+
+theory2(bt)
+theory2(bt + .01)
+theory2(bt - .01)
