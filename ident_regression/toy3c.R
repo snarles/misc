9ad3644b63ba5e0ced61cfd13c71b8d5f0eb7340
snarles <snarles@stanford.edu>
2015-04-01 15:19:30 -0700

    multivar

--- ident_regression/toy3c.R
+++ ident_regression/toy3c.R
@@ -39,14 +39,14 @@ simulate <- function(al, k_cl, n_trials, seed = 0) {
 }
 
 
-bts <- seq(0.9, 1.1, 0.01)
+bts <- seq(0.98, 1.02, 0.02)
 bts
 
 #mc2 <- sapply(bts, function(bt) mean(simulate(bt, 2, 1e4)))
-mc3 <- sapply(bts, function(bt) mean(simulate(bt, 3, 1e5)))
+mc3 <- sapply(bts, function(bt) mean(simulate(bt, 3, 1e6)))
 #mc5 <- sapply(bts, function(bt) mean(simulate(bt, 5, 1e4)))
 #mc10 <- sapply(bts, function(bt) mean(simulate(bt, 10, 1e4)))
-mc20 <- sapply(bts, function(bt) mean(simulate(bt, 20, 1e5)))
+mc20 <- sapply(bts, function(bt) mean(simulate(bt, 20, 1e6)))
 mc40 <- sapply(bts, function(bt) mean(simulate(bt, 40, 1e3)))
 #mc80 <- sapply(bts, function(bt) mean(simulate(bt, 80, 1e3)))
 min3 <-bts[mc3 == min(mc3)]
--- ident_regression/toy4.R
+++ ident_regression/toy4.R
@@ -1,13 +1,15 @@
 p <- 2
-bt <- 5 * matrix(1:4, 2, 2)
+q <- 2
+bt <- matrix(c(4,3,2,3), 2, 2)
+library(class)
 
 simulate <- function(al, k_cl, n_trials, seed = 0) {
   set.seed(seed)
   mrs <- numeric(n_trials)
   for (i in 1:n_trials) {
-    xs <- matrix(rnorm(p * n_trials), p, n_trials)
-    ys <- (bt %*% xs)[1, ]
-    yhats <- (al %*% xs)[1, ]
+    xs <- matrix(rnorm(p * k_cl), p, k_cl)
+    ys <- t(bt %*% xs + matrix(rnorm(q * k_cl), q, k_cl))
+    yhats <- t(al %*% xs)
     te_cl <- knn(t(t(yhats)), t(t(ys)), 1:k_cl, k = 1)
     mr <- sum(te_cl != 1:k_cl)/k_cl
     mrs[i] <- mr
@@ -19,10 +21,8 @@ del1 <- matrix(c(1, 0, 0, 0), 2, 2)
 del2 <- matrix(c(0, 1, 0, 0), 2, 2)
 del3 <- matrix(c(0, 0, 1, 0), 2, 2)
 del4 <- matrix(c(0, 0, 0, 1), 2, 2)
-
-
-n_trials < 1e4
-k_cl <- 3
+n_trials <- 1e4
+k_cl <- 30
 mean(simulate(bt, k_cl, n_trials))
 mean(simulate(bt + 0.1 * del1, k_cl, n_trials))
 mean(simulate(bt - 0.1 * del1, k_cl, n_trials))
