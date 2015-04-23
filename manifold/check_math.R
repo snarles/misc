c89818b3dd3e716b38c6b63fef418c784dd30b7b
Charles Zheng <snarles@stanford.edu>
2015-04-23 03:09:27 +0000

    error term analysis

--- manifold/check_math.R
+++ manifold/check_math.R
@@ -1,3 +1,23 @@
+orthocomplete <- function(a, b) {
+  d2 <- dim(a)[2]
+  ab <- cbind(a, b)
+  res <- qr(ab)
+  b2 <- qr.Q(res)[, -(1:d2)]
+  signs <- sign(apply(b2 * b, 2, sum))
+  b2 <- b2 %*% diag(signs)
+  cbind(a, b2)
+}
+
+align_sign <- function(a1, a2) {
+  temp <- t(a1) %*% a2
+  inds <- apply(temp, 1, function(v) { order(-abs(v))[1]  })
+  signs <- sign(temp[cbind(1:dim(a1)[2], inds)])
+  ans <- matrix(0, dim(a2)[2], dim(a2)[2])
+  ans[cbind(1:dim(a1)[2], inds)] <- signs
+  t(ans)
+}
+
+
 ## basic ridge PCR SVD expressions
 
 n <- 2000
@@ -24,13 +44,15 @@ y_pcr2 <- res$u %*% diag(c(rep(1, k), rep(0, p-k)) )%*% t(res$u) %*% y
 
 ## ridge loss approx
 
-p <- 20
-r <- 10
+p <- 5
+r <- 2
 apa <- matrix(rnorm(r * p), r, p)
+temp <- svd(apa)
+apa <- temp$u %*% diag(1:r) %*% t(temp$v)
 gma <- rnorm(r)
 
-n <- 40000
-l <- 20000
+n <- 80000
+l <- 40000
 z <- matrix(rnorm(n * r), n, r)
 sigma_e <- 1
 e_x <- sigma_e * matrix(rnorm(n * p), n, p)
@@ -64,21 +86,53 @@ err_term2 <- (1/n) * e_x %*%
   (res_x$v) %*% diag(1/(res_x$d^2 + lada)) %*% t(res_x$v) %*%
   t(apa) %*% t(z_l) %*% z_l %*% gma
 
-hist(yh_ridge2)
-hist(err_term1)
-hist(err_term2)
+v_a <- orthocomplete(res_a$v, res_x$v[, -(1:r)])
+permmat <- align_sign(res_x$v, v_a)
+v_a <- v_a %*% permmat
+d_a <- t(permmat) %*% diag(c(res_a$d, numeric(p - r))) %*% permmat
+eta <- cbind(res_a$u, matrix(0, r, p - r)) %*% permmat
+#apa2 <- eta %*% d_a %*% t(v_a)
+#sum(abs(apa - apa2))
 
+#image(t(v_a) %*% res_x$v)
+delta_v <- t(v_a) %*% res_x$v - diag(rep(1, p))
+#max(abs(diag(delta_v)))
+#image(delta_v)
+delta_z <- (t(z_l) %*% z_l/l) - diag(rep(1, r))
+#image(delta_z)
 
-yh_ridge[1]
-(yh_ridge-yh_ridge2)[1]
+yh_ridge3 <- (1/n) * z %*% eta %*% d_a %*% t(v_a) %*%
+  (res_x$v) %*% diag(1/(res_x$d^2 + lada)) %*% t(res_x$v) %*%
+  v_a %*% d_a %*% t(eta) %*% 
+  t(z_l) %*% z_l %*% gma
 
+#plot(yh_ridge3 + err_term1 + err_term2, yh_ridge2)
 
-resid_ridge <- yh_ridge - y
-dim(res_a$u)
-resid_ridge2 <- z %*% (res_a$u %*% diag(res_a$d^2/(res_x$d[1:r]^2 + lada)) %*% t(res_a$u) %*% gma - gma) - e_y
+error_term3 <- (1/n) * z %*% eta %*% d_a %*% delta_v %*%
+  diag(1/(res_x$d^2 + lada)) %*% t(res_x$v) %*%
+  v_a %*% d_a %*% t(eta) %*% 
+  t(z_l) %*% z_l %*% gma
 
+error_term4 <- (1/n) * z %*% eta %*% d_a %*% t(v_a) %*%
+  (res_x$v) %*% diag(1/(res_x$d^2 + lada)) %*% t(delta_v) %*%
+  d_a %*% t(eta) %*% 
+  t(z_l) %*% z_l %*% gma
 
+error_term5 <- z %*% eta %*% d_a %*% t(v_a) %*%
+  (res_x$v) %*% diag(1/(res_x$d^2 + lada)) %*% t(res_x$v) %*%
+  v_a %*% d_a %*% t(eta) %*% 
+  delta_z %*% gma
 
+hist(yh_ridge[-(1:l)])
+hist(err_term1[-(1:l)])
+hist(err_term2[-(1:l)])
 
-plot(resid_ridge[-(1:l)], resid_ridge2[-(1:l)])
-plot(resid_ridge, -e_y)
+hist(yh_ridge3[-(1:l)])
+hist(error_term3[-(1:l)])
+hist(error_term4[-(1:l)])
+hist(error_term5[-(1:l)])
+hist((error_term3 + error_term4 + error_term5)[-(1:l)])
+
+hist(delta_v)
+hist(delta_z)
+1/(res_x$d^2 + lada)
