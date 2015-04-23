41ceac73cdd0a098095ee2d702a0e8521f4108ba
Charles Zheng <snarles@stanford.edu>
2015-04-23 18:24:13 +0000

    look at personality data

--- manifold/pcr_data.R
+++ manifold/pcr_data.R
@@ -8,6 +8,11 @@ if ("ubuntu" %in% lf) (ddir <- "/home/ubuntu/stat312data")
 if ("rstudio" %in% lf) (ddir <- "/home/rstudio/stat312data")
 list.files(ddir)
 
+## questionnaire data
+qdata <- as.matrix(read.csv(paste0(ddir, "/data.csv"), header = TRUE, sep = '\t')[, 1:163]) %>% scale()
+filtered_resp <- qdata
+
+## fMRI data
 train_resp <- read.csv(paste0(ddir, "/train_resp_all.csv"), header = FALSE) %>%
   t()%>% scale()
 good_vox <- (apply(train_resp, 2, function(v) sum(is.na(v))) == 0)
@@ -50,15 +55,35 @@ pcr_error <- function(ks) {
     y_hat_pcr <- res$coefficients[1] + tt_tr2 %*% res$coefficients[-1]
     pcr_errs[i] <<- sum((y_hat_pcr - y_tr[inds_val])^2)
   }
-  k_sel <- order(pcr_errs)[1]
+  k_sel <- ks[order(pcr_errs)[1]]
+  print(paste("selected k:", k_sel))
   tt_tr <- x_tr %*% res_n$v[, 1:k_sel]
   tt_te <- x_te %*% res_n$v[, 1:k_sel]
   res <- lm(y_tr ~ tt_tr)
   y_hat_pcr <- res$coefficients[1] + tt_te %*% res$coefficients[-1]
-  (sqe_pcr <- sum((y_hat_pcr - y_te)^2))
+  (sqe_pcr <<- sum((y_hat_pcr - y_te)^2))
 }
 
-setup_problem(100)
-current_pars
-ridge_error()
-pcr_error(1:20)
+record_results <- function() {
+  current_pars$sqe_glmnet <<- sqe_glmnet
+  current_pars$sqe_pcr <<- sqe_pcr
+  all_cache <<- c(all_cache, list(current_pars))
+}
+
+reset_results <- function() {
+  all_cache <<- list()
+}
+
+
+plot(res_n$d)
+reset_results()
+
+for (i in 1:10) {
+  setup_problem(50)
+  #current_pars
+  ridge_error()
+  pcr_error(7)
+  record_results()  
+}
+
+all_cache
