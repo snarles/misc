dfe457e6fb1cbeabcff63f8363a50baf57cb14fb
Charles Zheng <snarles@stanford.edu>
2015-04-28 04:52:01 +0000

    metalearning

---
+++ manifold/pf16_data.R
@@ -0,0 +1,32 @@
+library(magrittr)
+library(dplyr)
+library(glmnet)
+
+lf <- list.files("/home")
+if ("snarles" %in% lf) (ddir <- "/home/snarles/mldata")
+if ("ubuntu" %in% lf) (ddir <- "/home/ubuntu/mldata")
+if ("rstudio" %in% lf) (ddir <- "/home/rstudio/mldata")
+list.files(ddir)
+
+## questionnaire data
+rawdata <- read.csv(paste0(ddir, "/pf16_data.csv"), header = TRUE, sep = '\t')
+q_inds <- 1:163
+o_inds <- 164:169
+
+temp <- read.csv(paste0(ddir, '/codebook.csv'), sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+codebook <- character(); codebook[temp[,1]] <- temp[, 2]
+
+protoset <- prototypes(rawdata[, q_inds], 10)
+
+
+temp <- table(rawdata$country) %>% {.[. > 5]} %>% sort %>% rev %>% names
+top_countries <- 1:length(temp)
+names(top_countries) <- temp
+datasplit <- top_countries[rawdata$country]
+sum(is.na(datasplit))
+sum(!is.na(datasplit))
+View(cbind(datasplit, rawdata$country)[1:100, ])
+x_inds <- c(names(rawdata)[o_inds], protoset) %>% setdiff("country")
+y_ind <- sample(setdiff(names(rawdata)[q_inds], protoset), 1)
+
+ds <- datasets(rawdata, x_inds, y_ind, datasplit)
