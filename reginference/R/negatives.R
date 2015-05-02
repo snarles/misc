4604915c5710680a2b002b8713820f85d0cee3de
Charles Zheng <snarles@stanford.edu>
2015-05-02 16:30:20 -0700

    more simulation code

--- reginference/R/negatives.R
+++ reginference/R/negatives.R
@@ -7,14 +7,22 @@
 #' @param name The column name
 #' @param adjoin Whether or not to adjoin the result z to x
 #' @return An (n x q) matrix or data frame of random projections.  The class of the output matches the class of the input.
-noised_projections <- function(x, sigma = 0.1, q = dim(x)[2], name = "Neg",
+noised_projections <- function(x, sigma = 0.1, q = dim(x)[2], name = NULL,
                                adjoin = FALSE) {
+  if (is.null(name)) {
+    if (sigma < Inf) name <- "Neg"
+    if (sigma == Inf) name <- "Noise"
+  }
   x0 <- scale(as.matrix(x), TRUE, TRUE)
   n <- dim(x0)[1]; p <- dim(x0)[2]
   coeffs <- matrix(rnorm(p * q), p, q)
-  z <- x0 %*% coeffs
-  z <- apply(z, 2, function(v) v/sqrt(sum(v^2)))
-  z <- z + sigma * matrix(rnorm(n * q), n, q)
+  if (sigma < Inf) {
+    z <- x0 %*% coeffs
+    z <- apply(z, 2, function(v) v/sqrt(sum(v^2)))
+    z <- z + sigma * matrix(rnorm(n * q), n, q)
+  } else {
+    z <- sigma * matrix(rnorm(n * q), n, q)
+  }
   z <- scale(z, TRUE, TRUE)
   colnames(z) <- paste(name, 1:q, sep = "")
   if (class(x) == "data.frame") z <- data.frame(z)
---
+++ reginference/src/pdist.cpp
@@ -0,0 +1,35 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+ 
+using namespace Rcpp;
+ 
+//' @title
+//' fastPdist2
+//' @description
+//' Pairwise distance functions
+//' 
+//' @param Ar first matrix
+//' 
+//' @param Br second matrix
+//' 
+//' @details
+//' \code{fastPdist} from http://blog.felixriedel.com/2013/05/pairwise-distances-in-r/
+//'
+//' @export
+// [[Rcpp::export]]
+NumericMatrix fastPdist2(NumericMatrix Ar, NumericMatrix Br) {
+    int m = Ar.nrow(), 
+        n = Br.nrow(),
+        k = Ar.ncol();
+    arma::mat A = arma::mat(Ar.begin(), m, k, false); 
+    arma::mat B = arma::mat(Br.begin(), n, k, false); 
+ 
+    arma::colvec An =  sum(square(A),1);
+    arma::colvec Bn =  sum(square(B),1);
+ 
+    arma::mat C = -2 * (A * B.t());
+    C.each_col() += An;
+    C.each_row() += Bn.t();
+ 
+    return wrap(sqrt(C)); 
+}
