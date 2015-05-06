6c066b6969dc29653edd4487f8d681c1bbca1330
Charles Zheng <snarles@stanford.edu>
2015-05-06 10:16:52 -0700

    more exposition

--- reginference_talk/tests.R
+++ reginference_talk/tests.R
@@ -3,6 +3,30 @@ library(magrittr)
 library(dplyr)
 library(glmnet)
 
+## prostate data
+data(prostate)
+dim(prostate)
+x <- as.matrix(prostate[, 1:8])
+y <- prostate[, 9]
+summary(lm(y ~ x))
+
+## generate random noise columns
+z <- matrix(rnorm(97 * 100), 97, 100)
+x2 <- data.frame(cbind(x, z))
+sigma_h <- sd(lm(y ~ as.matrix(x))$residuals)
+
+cors <- cor(x2, y)
+cors[order(-abs(cors)), , drop = F]
+
+lars(x2, y)
+
+library(covTest)
+covTest(lars(x, y), x, y)
+help(covTest)
+res <- covTest(lars(x2, y), x2, y, sigma.est = sigma_h)
+res
+
+
 
 
 ## galaxy data
@@ -13,8 +37,7 @@ gal_y <- galaxy[, 5]
 
 ## pf 16 data
 ## questionnaire data
-lf <- list.files("/home")
-if ("snarles" %in% lf) (ddir <- "/home/snarles/mldata")
+ddir <- "/home/snarles/mldata"
 rawdata <- read.csv(paste0(ddir, "/16PF/data.csv"), header = TRUE, sep = '\t')
 q_inds <- 1:163
 o_inds <- 164:169
@@ -23,13 +46,73 @@ pf_y <- rawdata$age
 filt <- pf_y < 100
 pf_x <- pf_x[filt, ]
 pf_y <- pf_y[filt]
-dim(pf_x) # 49159   163
-res_pf <- lm(pf_y ~ as.matrix(pf_x))
+filtdata <- rawdata[filt, ]
+dim(pf_x) # 49086   163
+f1 <- as.formula(paste("age ~ ", paste(colnames(pf_x), collapse = " + ")))
+f1
+res_pf <- lm(f1, data = filtdata)
+#res_pf <- lm(pf_y ~ as.matrix(pf_x))
+cm0 <- summary(res_pf)$coefficients[-1, ]
+cm <- cm0[order(cm0[, 1]), ]
+png("reginference_talk/pf16_coefs.png", width = 800, height = 650)
+par(mar = c (5.1, 7.1, 4.1, 2.1))
+plot(1:163, cm[, 1], xlab = "variable", ylab = expression(beta[i]), axes = FALSE, cex.lab = 3)
+abline(0, 0)
+for (i in 1:163) {
+  lines(c(i, i), cm[i, 1] + c(3, -3) * cm[i, 2], type = "o", pch = "-")
+}
+dev.off()
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+
+cm0
+signif <- (abs(cm0[, 1])  >  3 * cm0[, 2]) 
+sum(signif)
+min(abs(res_pf$coefficients[-1][signif]))
+f2 <- as.formula(paste("age ~ ", paste(colnames(pf_x)[signif], collapse = " + ")))
+f2
+res_pf2 <- lm(f2, data = filtdata)
+yh1 <- res_pf$coefficients[1] + as.matrix(pf_x) %*% res_pf$coefficients[-1]
+yh2 <- res_pf$coefficients[1] + as.matrix(pf_x[, signif]) %*% res_pf$coefficients[-1][signif]
+
+range(pf_y)
+png("reginference_talk/pf16_modelcheck.png", width = 700, height = 700)
+par(mar = c (5.1, 7.1, 4.1, 2.1))
+plot(yh1, yh2, ylim = c(0, 80), xlim = c(0, 80), pch = "+",
+     xlab = "full model", ylab = "sel. model", cex.lab = 2, cex.axis = 2)
+abline(0, 1, col = "red", lwd = 2)
+dev.off()
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+
+subs <- pf_x[, sample(163, 15)]
+res_sub <- lm(pf_y ~ as.matrix(subs))
+cfs <- summary(res_sub)$coefficients[-1, ]
+subsel <- abs(cfs[, 1]) > 3 * cfs[, 2]
+sum(subsel)
+png("reginference_talk/pf16_modelcheck2.png", width = 700, height = 700)
+par(mar = c (5.1, 7.1, 4.1, 2.1))
+yh1 <- res_sub$coefficients[1] + as.matrix(subs) %*% res_sub$coefficients[-1]
+yh2 <- res_sub$coefficients[1] + as.matrix(subs[, subsel]) %*% res_sub$coefficients[-1][subsel]
+plot(yh1, yh2, ylim = c(0, 80), xlim = c(0, 80), pch = "+",
+     xlab = "full model", ylab = "sel. model", cex.lab = 2, cex.axis = 2)
+abline(0, 1, col = "red", lwd = 2)
+dev.off()
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+
+sh <- cov(pf_x)
+n <- dim(pf_x)[1]
+vs <- diag(solve(sh))
+plot(sort(vs))
+
 hist(res_pf$residuals)
+var(res_pf$residuals)
+max(abs(res_pf$coefficients[-1]))
+lapply(pf_x, var)
 plot(res_pf$residuals, res_pf$fitted.values)
 plot(res_pf$fitted.values, pf_y)
 res <- loess(pf_y ~ res_pf$fitted.values)
 plot(res$x, res$fitted)
+
+
 
 ## HIV data
 drug_class = 'PI' # Possible drug types are 'PI', 'NRTI', and 'NNRTI'.
