693d919d7149dfa31e77d1e2fec1042e9df671bb
Charles Zheng <snarles@stanford.edu>
2015-05-06 20:47:38 -0700

    more tests

--- reginference_talk/tests4.R
+++ reginference_talk/tests4.R
@@ -40,4 +40,13 @@ res_k_HIV <- knockoff_results(x_HIV, y_HIV)
 res_k_gal <- knockoff_results(x_gal, y_gal)
 
 
+save(file = paste0("test4_", seed, ".RData"),
+     list = c("res_c_pf16", "res_c_fMRI", "res_c_HIV", "res_c_gal",
+              "res_s_pf16", "res_s_fMRI", "res_s_HIV", "res_s_gal",
+              "res_k_pf16", "res_k_fMRI", "res_k_HIV", "res_k_gal"))
+
+good_pf16 <- max(c(res_c_pf16[50, 3], res_s_pf16[50, 3], res_k_pf16[50, 3]))
+good_fMRI <- max(c(res_c_fMRI[50, 3], res_s_fMRI[50, 3], res_k_fMRI[50, 3]))
+good_HIV <- max(c(res_c_HIV[50, 3], res_s_HIV[50, 3], res_k_HIV[50, 3]))
+good_gal <- max(c(res_c_gal[50, 3], res_s_gal[50, 3], res_k_gal[50, 3]))
 
