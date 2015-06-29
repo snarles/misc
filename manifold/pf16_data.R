65cc87fecb10dde2ac4b1c1ea74604d9923db58b
snarles <snarles@stanford.edu>
2015-06-29 12:48:20 -0700

    new files

--- manifold/pf16_data.R
+++ manifold/pf16_data.R
@@ -9,11 +9,12 @@ if ("rstudio" %in% lf) (ddir <- "/home/rstudio/mldata")
 list.files(ddir)
 
 ## questionnaire data
-rawdata <- read.csv(paste0(ddir, "/pf16_data.csv"), header = TRUE, sep = '\t')
+rawdata <- read.csv(paste0(ddir, "/16PF/data.csv"), header = TRUE, sep = '\t')
 q_inds <- 1:163
 o_inds <- 164:169
+saveRDS(rawdata, "16pf.rds")
 
-temp <- read.csv(paste0(ddir, '/codebook.csv'), sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+temp <- read.csv(paste0(ddir, '/16PF/codebook.csv'), sep = '\t', header = FALSE, stringsAsFactors = FALSE)
 codebook <- character(); codebook[temp[,1]] <- temp[, 2]
 
 protoset <- prototypes(rawdata[, q_inds], 10)
