144a05cfa445f0e970bc31a356aa93c899c4d4dc
snarles <snarles@stanford.edu>
2015-07-27 11:21:04 -0700

    description of linear type CAR

--- stats253/hw3.R
+++ stats253/hw3.R
@@ -11,6 +11,7 @@ library(RColorBrewer)
 library(classInt)
 library(magrittr)
 library(spdep)
+library(pracma)
 
 shapes <- readShapeSpatial("scotlip/scotlip.shp")
 plot(shapes)
@@ -21,6 +22,41 @@ nb <- poly2nb(shapes)
 shapes@data %>% names
 #[1] "CODENO"    "AREA"      "PERIMETER" "RECORD_ID" "DISTRICT"  "NAME"      "CODE"      "CANCER"    "POP"      
 #[10] "CEXP"      "AFF"    
-
 shapes@data %$% plot(POP, CANCER)
+
+pairs(shapes@data)
+
+## prune empty NB
+shapes@data %>% dim
+length(nb)
+sapply(nb, length)
+
+help(nb2mat)
+W <- nb2mat(nb, zero.policy=TRUE)
+W[1:10, 1:10]
+rowSums(W)
+diag(W)
+W2 <- (W * lower.tri(W)) %>% {. + t(.)}
+W2[1:10, 1:10]
+res <- eigen(W2)
+plot(res$values)
+sort(res$values)
+
+V <- res$vectors
+d <- res$values
+f2 <- function(x, y = 0) sum((x-y)^2)
+f2(W2, V %*% (d * t(V)))
+
+n <- dim(W)[1]
+phi <- 0.1
+f2(solve(eye(n) - phi * W2), V %*% (1/(1 - phi * d) * t(V)))
+
+y <- shapes@data$CANCER
+X <- shapes@data %$% cbind(POP, CEXP, AFF, AREA, PERIMETER)
+colnames(X)
+
+
+
+
+
 
