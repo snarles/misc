f304b70c7fd433c4c8fe82d6bd397de20eceb579
snarles <snarles@stanford.edu>
2015-08-20 16:05:19 -0700

    random energy model plots

--- misc2/random_energy.R
+++ misc2/random_energy.R
@@ -26,30 +26,95 @@ exp_mult <- function(delta) {
 }
 
 cauchy_samp <- function(delta, n) {
-  
+  ans <- c()
+  mult <- 1/cauchy_mult(delta)
+  while (length(ans) < n) {
+    xs <- rcauchy(n)
+    probs <- mult * exp(-(abs(xs)^delta))/(1 + xs^2)
+    filt <- (probs > runif(n))
+    ans <- c(ans, xs[filt])
+  }
+  ans <- ans[1:n]
+  ans
 }
 
 exp_samp <- function(delta, n) {
-  
+  ans <- c()
+  mult <- 1/exp_mult(delta)
+  while (length(ans) < n) {
+    xs <- rexp(n)
+    probs <- mult * exp(-(abs(xs)^delta - xs))
+    filt <- (probs > runif(n))
+    ans <- c(ans, xs[filt])
+  }
+  ans <- ans[1:n]
+  ans <- ans * sign(runif(n) - .5)
+  ans
 }
 
 #eta_function(2) - sqrt(pi)
 #eta_function(1) - 2
 
-
 ## delta < 1: Rejection sampling via Cauchy
-delta <- 0.5
-eta <- eta_function(delta)
-xs <- seq(0, 10, 0.1)
-plot(xs, exp(-(xs ^ delta))/eta, type = "l", col = "red")
-lines(xs, pi * cauchy_mult(delta)/eta * dcauchy(xs))
+xs <- cauchy_samp(0.5, 1000)
+hist(xs, breaks=40)
 
 ## delta < 2: Rejection sampling via exponential
-delta <- 1.5
-eta <- eta_function(delta)
-xs <- seq(0, 10, 0.1)
-plot(xs, exp(-(xs ^ delta))/eta, type = "l", col = "red")
-lines(xs, exp_mult(delta)/eta * exp(-xs))
+xs <- exp_samp(1.5, 1000)
+hist(xs, breaks=40)
+
+####
+##  Partition functions
+####
+
+betas <- 1/seq(0.01, 2, 0.01)
+
+Eis <- function(delta, N) {
+  if (delta < 1) {
+    Ei <- N^((delta-1)/(delta)) * cauchy_samp(delta, 2 ^ N)    
+  } else if (delta == 1) {
+    Ei <- rexp(2 ^ N)    
+  } else if (delta < 2) {
+    Ei <- N^((delta-1)/(delta)) * exp_samp(delta, 2^N)    
+  } else if (delta == 2) {
+    Ei <- N^((delta-1)/(delta)) * rnorm(2^N)
+  }
+  Ei
+}
+
+phis <- function(delta, N, betas) {
+  phi <- function(beta) (1/N) * logsumexp(-beta * Ei)
+  Ei <- Eis(delta, N)
+  phis <- sapply(betas, phi)
+  phis
+}
+
+triplot <- function(delta, betas, yl = c(-10, 10), Ns) {
+  plot(1/betas, -phis(delta, Ns[1], betas)/betas, type = "l", ylim = yl)
+  for (N in Ns) {
+    lines(1/betas, -phis(delta, N, betas)/betas,
+          lwd = 3 * N/max(Ns))
+  }
+}
+
+phiplot <- function(delta, betas, yl = c(-10, 10), Ns) {
+  plot(betas, phis(delta, Ns[1], betas), type = "l", ylim = yl)
+  for (N in Ns) {
+    lines(betas, phis(delta, N, betas),
+          lwd = 3 * N/max(Ns))
+  }
+}
 
 
+Ns <- c(5, 10, 15)
 
+triplot(0.5, 1/seq(0.01, 0.5, 0.01), c(-1, 1), Ns)
+triplot(1, betas, c(-1, 1), Ns)
+triplot(1.5, betas, c(-1, 0), Ns)
+triplot(2, betas, c(-2, 0), Ns)
+
+
+phiplot(0.5, seq(0, 2, 0.01), c(-1, 1), Ns)
+phiplot(1, seq(0, 2, 0.01), c(-1, 1), Ns)
+phiplot(1.5, seq(0, 2, 0.01), c(0, 2), Ns)
+phiplot(2, seq(0, 2, 0.01), c(0, 2), Ns)
