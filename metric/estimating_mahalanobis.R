090a8fe5ca76d3ce4ebbefc056db3978b56d2d14
snarles <snarles@stanford.edu>
2015-09-21 15:22:40 -0700

    cor(true mah, est mah) = sqrt(1-(p/n))

--- metric/estimating_mahalanobis.R
+++ metric/estimating_mahalanobis.R
@@ -8,12 +8,15 @@ get_cov <- function(n, p) {
   lambda <- eigen(w)$values
   z <- x %*% diag(sqrt(lambda))
   norms_z <- apply(z, 1, function(x) sum(x ^ 2))
-  res1 <- c(cov(norms_x, norms_z), 2 * n * p/(n - p - 1))
-  res2 <- c(var(norms_x), 2 * p)  
+  gam <- p/n
+  res1 <- c(cov(norms_x, norms_z), 2 * n * p/(n - p - 1), 2 * n * gam/(1 - gam))
+  res2 <- c(var(norms_x), 2 * p, 2 * gam * n)  
   res3 <- c(var(norms_z),
-            2 * n^2/((n-p)*(n-p-1)*(n-p-3)) * ((n-p-2) * p + p^2 + p))#,
+            2 * n^2/((n-p)*(n-p-1)*(n-p-3)) * ((n-p-2) * p + p^2 + p),
+            2 * gam * (n + 1)/(1 - gam)^3)#,
             #2 * n^2 * eTrWi2(n, eye(p)))
-  res4 <- c(cor(norms_x, norms_z), res1[2]/sqrt(res2[2] * res3[2]))
+  res4 <- c(cor(norms_x, norms_z), res1[2]/sqrt(res2[2] * res3[2]),
+            sqrt(1-gam))
   list(res1, res2, res3, res4)
 }
 
