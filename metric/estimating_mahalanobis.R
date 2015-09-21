23889b62c56a4d4387463c5883f21d55b2f724eb
snarles <snarles@stanford.edu>
2015-09-21 15:09:24 -0700

    explicit formula for special case

--- metric/estimating_mahalanobis.R
+++ metric/estimating_mahalanobis.R
@@ -9,14 +9,16 @@ get_cov <- function(n, p) {
   z <- x %*% diag(sqrt(lambda))
   norms_z <- apply(z, 1, function(x) sum(x ^ 2))
   res1 <- c(cov(norms_x, norms_z), 2 * n * p/(n - p - 1))
-  res2 <- c(var(norms_x), 2 * p)
-  res3 <- c(var(norms_z), 2 * n^2 * eTrWi2(n, eye(p)))
+  res2 <- c(var(norms_x), 2 * p)  
+  res3 <- c(var(norms_z),
+            2 * n^2/((n-p)*(n-p-1)*(n-p-3)) * ((n-p-2) * p + p^2 + p))#,
+            #2 * n^2 * eTrWi2(n, eye(p)))
   res4 <- c(cor(norms_x, norms_z), res1[2]/sqrt(res2[2] * res3[2]))
   list(res1, res2, res3, res4)
 }
 
 
-get_cov(20, 10)
+get_cov(200, 100)
 
 
 
