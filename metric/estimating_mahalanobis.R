fd5c9a9c5fec1cca539088e2377404582a15329b
snarles <snarles@stanford.edu>
2015-09-21 15:10:24 -0700

    cleanup

--- metric/estimating_mahalanobis.R
+++ metric/estimating_mahalanobis.R
@@ -20,22 +20,3 @@ get_cov <- function(n, p) {
 
 get_cov(200, 100)
 
-
-
-n <- 1000 # number of samples
-K <- 10 # number of p's to try out
-r <- 20 # number of reps per dimension
-cors_data <- vector(length = K, mode = "list")
-ps <- seq(n * 0.01, n, length.out = K)
-for(k in seq_len(K)) {
-  print(k)
-  cors_data[[k]] <- data.frame(n = n, p = ps[k], cors = replicate(r, get_cor(n, ps[k])))
-}
-library("data.table")
-cors_data <- rbindlist(cors_data)
-
-library("plyr")
-library("ggplot2")
-ddply(cors_data, .(n, p), summarize, mean = mean(cors), var = var(cors))
-ggplot(cors_data) +
-  geom_jitter(aes(x = p, y = cors))
