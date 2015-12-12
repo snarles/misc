616513bba10f7a593014509dba27dfb288887013
Charles Zheng <snarles@stanford.edu>
2015-12-12 06:27:09 -0800

    CV pipeline

--- cv_pipeline/cv_source.R
+++ cv_pipeline/cv_source.R
@@ -12,8 +12,12 @@ library(rARPACK)
 ## Feature extraction subroutines
 ####
 
+## does not do feature selection (returns all variables)
+extract_all <- function(X, opts = NULL) diag(rep(1, dim(X)[2]))
+
 ## selects k variables (columns) with highest marginal variances
 extract_high_variance <- function(X, opts = NULL) {
+  X <- as.matrix(X)
   p <- dim(X)[2]
   ## implement default arguments this way for compatibility
   if (is.null(opts)) opts <- list(k = 10)
@@ -32,6 +36,7 @@ extract_high_variance <- function(X, opts = NULL) {
 
 ## returns k principal directions (PCA), plus standardization constants
 extract_pcs <- function(X, opts = NULL) {
+  X <- as.matrix(X)
   ## implement default arguments this way for compatibility
   if (is.null(opts)) opts <- list(k = 10, standardize = TRUE)
   k <- opts$k
@@ -60,31 +65,66 @@ extract_pcs <- function(X, opts = NULL) {
 ####
 
 ## fits OLS and returns the fit object
-fit_ols <- function(X, y, opts = NULL) {
+fit_ols <- function(data, opts = NULL) {
   ## implement default arguments this way for compatibility
   if (is.null(opts)) opts <- list(intercept = TRUE)
   if (opts$intercept) {
-    res <- lm(y ~ X)
+    res <- lm(y ~ X, data = data)
   } else {
-    res <- lm(y ~ X + 0)    
+    res <- lm(y ~ X + 0, data = data)
   }
   res
 }
 
 
 ####
-##  Prediction and cross-validation pipelines
+##  Prediction and cross-validation procedures
 ##------------------------------------------
-##  Each 'pipeline' takes a training dataset and possibly a test dataset,
+##  Each procedure takes a training dataset and possibly a test dataset,
 ##  a specification of a linear feature extraction subroutine and a model fitting subroutine
 ##  and produces some kind of average MSE generalization error estimate.
-##   * generalization_error_pipeline:
+##   * test_error:
 ##       Estimates the generalization error of the method using training and test data.
-##   * full_cv_pipeline
+##   * full_cv_error:
 ##       Estimates generalization error using cross-validation,
 ##       performing feature extraction separately within each fold.
-##   * partial_cv_pipeline
+##   * partial_cv_error:
 ##       Estimates generalization error using CV, but using features from the full training data!
+##  INPUTS: Data (y, X) should be in a matrix, with the response y as the first column
+##  ARGUMENTS: 
+##             (1) `extractor`: Feature extraction subroutine,
+##             (2) `extract_opts`: options for feature extraction subroutine
+##             (3) `fitter`: Model fitting subroutine
+##             (4) `fit_opts`: options for moddel fitting subroutine
+##             (5) `train_data`: training data,
+##             and additional arghuments depending on procedure
 ####
+
+test_error <- function(extractor=extract_all,
+                       extract_opts = NULL, 
+                       fitter = fit_ols, 
+                       fit_opts = NULL, 
+                       train_data, test_data) {
+  train_data <- as.matrix(train_data)
+  test_data <- as.matrix(test_data)
+  y_tr <- train_data[, 1]
+  X_tr <- train_data[, -1]
+  ## extract features
+  dirs <- extractor(X_tr, extract_opts)
+  Z <- X_tr %*% dirs
+  trdat <- data.frame(y = y, X = I(Z))
+  ## fit model
+  res <- fitter(trdat, opts = fit_opts)
+  ## predict  
+  y_te <- test_data[, 1]
+  X_te <- test_data[, -1]
+  Z_te <- X_te %*% dirs
+  tedat <- data.frame(y = y_te, X = I(Z_te))
+  yhat <- predict(res, tedat)
+  ## mse
+  mean((yhat - y_te)^2)
+}
+
+
 
 
--- cv_pipeline/cv_source_test.R
+++ cv_pipeline/cv_source_test.R
@@ -9,12 +9,23 @@ source('cv_pipeline/cv_source.R')
 
 library(pracma)
 n <- 200
+n_te <- 20
 p <- 12
 X <- randn(n, p)
+X2 <- randn(n_te, p)
+
 bt <- rnorm(p)
 y <- X %*% bt + rnorm(n)
+y2 <- X2 %*% bt + rnorm(n_te)
+
+res <- lm(y ~ X)
+predict(res, X2)
 
 extract_high_variance(X)
 
 extract_pcs(X)
 
+train_data <- cbind(y, X)
+test_data <- cbind(y2, X2)
+
+test_error(train_data = train_data, test_data = test_data)
