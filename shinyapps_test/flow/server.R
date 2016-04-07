91aa5454a74d9f865b56c346df9ddf2c61112079
Charles Zheng <snarles@stanford.edu>
2016-04-06 18:27:11 -0700

    try navpanel

--- shinyapps_test/flow/server.R
+++ shinyapps_test/flow/server.R
@@ -4,12 +4,5 @@
 
 game_state <- list(tab = "main")
 
-shinyServer(function(input, output) {
-  output$ui <- renderUI({
-    if (game_state$tab == "main") {
-      return(
-        textInput("name", "Enter your name", value = "anon")
-      )
-    }
-  })
+shinyServer(function(input, output, session) {
 })
--- shinyapps_test/flow/ui.R
+++ shinyapps_test/flow/ui.R
@@ -2,24 +2,15 @@
 ##  Sequence of menus, updating data also
 ####
 library(shiny)
-shinyUI(fluidPage(
-  titlePanel("Choose your own adventure"),
-  fluidRow(
-    
-    column(3, wellPanel(
-      uiOutput("ui")
-    )),
-
-    column(3, wellPanel(
-      uiOutput("ui2")
-    )),
-    
-    column(3,
-           verbatimTextOutput("input_type_text")
-    ),
-    
-    column(3, wellPanel(
-      actionButton("submitted", label = "OK")
-    ))
-  )
+shinyUI(navbarPage("ROBUILDER",
+  navbarMenu("Game",
+    tabPanel("New",
+      radioButtons("new_game_confirm", "Confirm new game?", c("No", "Yes"))),
+    tabPanel("Save",
+      textInput("save_file", "Filename?")),
+    tabPanel("Load",
+             textInput("load_file", "Filename?"))
+  ),
+  tabPanel("Build",
+           textInput("lala","LALA"))
 ))
