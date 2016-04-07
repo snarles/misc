9811ab7bb864dcda6cf9141d261d30c723553aaa
Charles Zheng <snarles@stanford.edu>
2016-04-06 22:35:12 -0700

    got the desc table to work

--- shinyapps_test/flow/server.R
+++ shinyapps_test/flow/server.R
@@ -4,7 +4,7 @@
 
 game_state <- list(tab = "main")
 empty_army <- data.frame(name = "", cost = "", HP = "", Armor = "",
-                            EN = "", repair = "", attack = "", dmg = "",
+                            EN = "", recover = "", repair = "", attack = "", dmg = "",
                             en_cost = "", charge = "")
 empty_army <- empty_army[FALSE, ]
 starting_funds <- 100
@@ -12,6 +12,35 @@ starting_funds <- 100
 cost_formula <- function(hp, armor, en, atk1_lv, atk1_charge, atk2_lv, atk2_charge, standby_en,
                          repair_time) {
   hp + 5 * armor + en + 2 * atk1_lv + 2 * atk2_lv + 3 * standby_en + floor(60/repair_time)
+}
+
+atk_dmg <- function(atk_lv, atk_charge) atk_lv * (atk_charge + 1)
+atk_en <- function(atk_lv, atk_charge) 10
+
+desc_table <- function(name, hp, armor, en, 
+                       atk1_name, atk1_lv, atk1_charge, 
+                       atk2_name, atk2_lv, atk2_charge, standby_en,
+                 repair_time, cost, atkdmgs, atken) {
+  # cst <- cost_formula(hp, armor, en, atk1_lv, atk1_charge,
+  #                     atk2_lv, atk2_charge,
+  #                     standby_en, repair_time)
+  blanks <- rep("", 3);
+  atknames <- c(atk1_name, atk2_name)
+  # atkdmgs <- paste(c(atk_dmg(atk1_lv, atk1_charge), atk_dmg(atk2_lv, atk2_charge)))
+  atkcharges <- paste(c(atk1_charge, atk2_charge))
+  # atken <- paste(c(atk_en(atk1_lv, atk1_charge), atk_en(atk2_lv, atk2_charge)))
+  ans <- data.frame(Name = c(name, blanks),
+             Cost = c(paste0("$", cost), blanks),
+             HP = c(paste(hp), blanks),
+             Armor = c(paste(armor), blanks),
+             EN = c(paste(en), blanks),
+             repair = c(paste(repair_time), blanks),
+             Attack = c("", "Recover", atknames),
+             ENcost = c("", paste0("-",standby_en), atken),
+             dmg = c("", "0", atkdmgs),
+             charge_time = c("", "0", atkcharges))
+  filt <- c(TRUE, TRUE, atk1_lv > 0, atk2_lv > 0)
+  ans[filt, ]
 }
 
 shinyServer(function(input, output, session) {
@@ -27,14 +56,19 @@ shinyServer(function(input, output, session) {
     output$army2_comment <- renderText(paste0(game_state$names[2], "'s army:"))
     output$build_turn_message <- renderText(paste0("It is ", game_state$names[1], "'s turn to design a prototype!"))
     output$build_funds_message <- renderText(paste0("You have $", game_state$funds[game_state$current]))
-    output$build_cost <- renderText(cost())
+    output$build_cost <- renderTable(desc())
   })
-  cost <- reactive({
-    paste0("This unit will cost $", 
-      cost_formula(hp=input$hp, armor=input$armor, en=input$en, 
-                   atk1_lv = input$atk1_lv, atk1_charge = input$atk1_charge,
-                   atk2_lv = input$atk2_lv, atk2_charge = input$atk2_charge,
-                   standby_en = input$standby$en, repair_time = input$repair_time),
-      " to build in game.")
+  desc <- reactive({
+    desc_table(name = input$name, hp=input$hp, armor=input$armor, en=input$en, atk1_name = input$atk1_name,
+                        atk1_lv = input$atk1_lv, atk1_charge = input$atk1_charge, atk2_name = input$atk2_name,
+                        atk2_lv = input$atk2_lv, atk2_charge = input$atk2_charge,
+                        standby_en = input$standby$en, repair_time = input$repair_time,
+               cost = cost_formula(hp = input$hp, en = input$en, armor = input$armor,
+                                   atk1_lv = input$atk1_lv,atk1_charge = input$atk1_charge,
+                                   atk2_lv = input$atk2_lv,atk2_charge = input$atk2_charge,
+                                   standby_en = input$atk1_lv,repair_time = input$repair_time),
+               atkdmgs = paste(c(atk_dmg(input$atk1_lv, input$atk1_charge), atk_dmg(input$atk2_lv, input$atk2_charge))),
+               atken = paste(c(atk_en(input$atk1_lv, input$atk1_charge), atk_en(input$atk2_lv, input$atk2_charge)))
+               )
   })
 })
