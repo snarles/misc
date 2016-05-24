12eac059c49585476f540f7301ae96a4876c28e1
Charles Zheng <snarles@stanford.edu>
2016-05-24 11:43:11 -0700

    some theory of local regression

--- grf_kernels.R
+++ grf_kernels.R
@@ -3,15 +3,18 @@
 library(pracma)
 library(MASS)
 
-hh <- 5 # bandwidth for GRF
-v0 <- 10 # variance for global mean
+hh <- 1 # bandwidth for GRF
+v0 <- 100 # variance for global mean
+vX <- 100 # variance for linear effect
+vN <- 10 # individual variance (nugget effect)
 
 n <- 500 # number of points
 p <- 2 # dimension
 
 xs <- randn(n, p)
 dd <- pdist(xs)
-covm <- v0 + exp(-dd/hh)
+covm <- v0 + exp(-dd/hh) + vN * eye(n) + 
+  vX * (xs[, 1] %*% t(xs[, 1])) + vX * (xs[, 2] %*% t(xs[, 2]))
 ys <- mvrnorm(1, rep(0, n), covm)
 
 dev.new("X11", width = 8, height = 6, noRStudioGD = TRUE)
@@ -27,9 +30,9 @@ while(!is.null(xx)) {
   barplot(sort(wts), ylim = c(-1/sqrt(n), 10/sqrt(n)))
   plot(xs, axes = FALSE, ann = FALSE, asp = 1, col = "white")
   for (i in 1:(n-1)) {
-    if (wts[i] > 0) cl <- grey(level = 1 - pmax(0, wts[i]/max(wts)))
-    if (wts[i] < 0) cl <- rgb(1 +wts[i]/max(wts), 1, 1+wts[i]/max(wts))
-    points(xs[-ind, ][i, , drop = FALSE], cl = cl)
+    if (wts[i] >= 0) cl <- grey(level = 1 - pmax(0, wts[i]/max(wts)))
+    if (wts[i] < 0) cl <- rgb(1 -wts[i]/min(wts), 1, 1-wts[i]/min(wts))
+    points(xs[-ind, ][i, , drop = FALSE], col = cl)
   }
   points(xs[ind, , drop = FALSE], col = "red")
   xx <- locator(1)
