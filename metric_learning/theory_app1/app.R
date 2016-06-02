05e0de5149e7e7f13810b78baeeff89e3a6d4174
snarles <snarles@stanford.edu>
2016-06-02 13:01:33 -0700

    completed the app

--- metric_learning/theory_app1/app.R
+++ metric_learning/theory_app1/app.R
@@ -5,6 +5,12 @@ KERNEL_CHOICES <- c("Optimal", "LR_knn", "LR_unif", "LR_gauss")
 
 v0 <- 100 # variance for global mean
 
+wts_lr <- function(xs, wts, x) {
+  xwx <- t(xs) %*% diag(wts) %*% xs
+  xw <- t(xs) %*% diag(wts)
+  as.numeric(x %*% solve(xwx, xw))
+}
+
 shinyApp(
   ui = fluidPage(
     # Some custom CSS
@@ -34,7 +40,7 @@ shinyApp(
              div(class = "option-group",
                  div(class = "option-header", "Generation"),
                  numericInput("par_lambda", "Lambda: ", 1.5),
-                 numericInput("par_lin", "Linearity: ", 99),
+                 selectInput("par_lin", "Linearity: ", c(0, 99), selected = 99),
                  numericInput("par_n", "N: ", 20),
                  numericInput("par_s0", "Individual sigma: ", 5.2),
                  numericInput("par_seed", "Random seed: ", 555)
@@ -49,13 +55,10 @@ shinyApp(
              )
       ),
       column(width = 4,
-             uiOutput("plotUI1"))
-    ),
-    fluidRow(
-      column(width = 4,
-             uiOutput("plotUI2")),
-      column(width = 4,
-             uiOutput("plotUI3"))
+             div(
+             uiOutput("plotUI1"),
+             uiOutput("plotUI2"),
+             uiOutput("plotUI3")))
     )
   ),
   
@@ -66,33 +69,54 @@ shinyApp(
       xs
     })
     curWeights <- reactive({
-      ans <- rep(1, input$par_n)/input$par_n
+      n <- input$par_n
+      nn <- input$par_knn
+      vN <- input$par_s0^2
+      vX <- as.numeric(input$par_lin)
+      hh <- input$par_lambda
+      ans <- rep(1, n)/n
+      xs <- curPoints()
       if (!is.null(input$plot1_click)) {
         p1c <- t(c(input$plot1_click$x, input$plot1_click$y))
         if (input$choice_k == "Optimal") {
-          xs <- curPoints()
           xs2 <- rbind(xs, p1c)
           dd0 <- pdist(xs2)
-          vN <- input$par_s0^2
-          vX <- input$par_lin
-          hh <- input$par_lambda
-          n <- input$par_n
-          covm <- v0 + exp(-dd0/hh) + vN * eye(n + 1) + 
+          covm <- v0 + exp(-dd0/(hh^2)) + vN * eye(n + 1) + 
             vX * (xs2[, 1] %*% t(xs2[, 1])) + vX * (xs2[, 2] %*% t(xs2[, 2]))
           ans <- as.numeric(solve(covm[-(n+1), -(n+1)], covm[-(n+1), (n+1)]))
+        }
+        if (input$choice_k == "LR_knn") {
+          dd <- as.numeric(pdist2(p1c, xs))
+          inds <- which(dd <= sort(dd)[nn])
+          ans <- numeric(n)
+          xsi <- xs[inds, , drop = FALSE]
+          px <- p1c %*% solve(t(xsi) %*% xsi, t(xsi))
+          ans[inds] <- px
+        }
+        if (input$choice_k == "LR_unif") {
+          dd <- as.numeric(pdist2(p1c, xs))
+          inds <- which(dd <= input$par_h)
+          ans <- numeric(n)
+          xsi <- xs[inds, , drop = FALSE]
+          px <- p1c %*% solve(t(xsi) %*% xsi, t(xsi))
+          ans[inds] <- px
+        }
+        if (input$choice_k == "LR_gauss") {
+          dd <- as.numeric(pdist2(p1c, xs))
+          ans <- wts_lr(xs, exp(-dd/(input$par_h)^2), p1c)
         }
       }
       ans
     })
     output$plotUI1 <- renderUI({
-      plotOutput("plot1", height = 350,
+      plotOutput("plot1", height = 300,
                  click = "plot1_click")
     })
     output$plotUI2 <- renderUI({
-      plotOutput("plot2", height = 350)
+      plotOutput("plot2", height = 300)
     })
     output$plotUI3 <- renderUI({
-      plotOutput("plot3", height = 350)
+      plotOutput("plot3", height = 200)
     })
     output$plot1 <- renderPlot({
       plot(curPoints(), xlab = "x", ylab = "y")
