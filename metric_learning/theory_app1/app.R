78917b144f177de9946d8c2c94da7e974d50daa4
Charles Zheng <snarles@stanford.edu>
2016-06-02 10:19:19 -0700

    point generation

--- metric_learning/theory_app1/app.R
+++ metric_learning/theory_app1/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(pracma)
 
 KERNEL_CHOICES <- c("Optimal", "LR_knn", "LR_unif", "LR_gauss")
 
@@ -35,9 +36,7 @@ shinyApp(
                  numericInput("par_s0", "Individual sigma: ", 5.2),
                  numericInput("par_seed", "Random seed: ", 555)
              )
-      )
-    ),
-    fluidRow(
+      ),
       column(width = 3,
              div(class = "option-group",
                  div(class = "option-header", "Kernel"),
@@ -45,11 +44,25 @@ shinyApp(
                  numericInput("par_knn", "Neighbors: ", 5),
                  radioButtons("choice_k", "Kernel: ", KERNEL_CHOICES, selected = 1)
              )
-      )
+      ),
+      column(width = 4,
+             uiOutput("plotUI1"))
     )
   ),
   
   server = function(input, output, session) {
+    curPoints <- reactive({
+      set.seed(input$par_seed)
+      xs <- randn(input$par_n, 2)
+      xs
+    })
+    output$plotUI1 <- renderUI({
+      plotOutput("plot1", height = 350,
+                 click = "plot1_click")
+    })
+    output$plot1 <- renderPlot({
+      plot(curPoints(), xlab = "x", ylab = "y")
+    })
   }
 )
 
