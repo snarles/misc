cec0912ab3edcb08cca2844200f201aea744f78b
Charles Zheng <snarles@stanford.edu>
2016-08-19 15:33:27 -0600

    correction

--- misc2/dad3.R
+++ misc2/dad3.R
@@ -4,11 +4,18 @@ library(readxl)
 
 tab <- readxl::read_excel("misc2/CrossLorenz.xlsx")
 tab <- tab[1:18, -11]
-lapply(tab, class)
+#lapply(tab, class)
 results <- numeric()
+results2 <- numeric()
 nms <- character()
 for (i in 1:9) {
   nms[i] <- paste(tab[2 * i - 1, 1], tab[2 *i, 1], sep = "v")
-  results[i] <- theprob(as.numeric(tab[2 * i - 1, -1]), as.numeric(tab[2 *i, -1]))
+  xdiff <- as.numeric(tab[2 * i - 1, -1]) - as.numeric(tab[2 *i, -1])
+  xdiff <- xdiff/sd(xdiff)
+  results[i] <- empprob(xdiff[xdiff > 0], -xdiff[xdiff < 0], 1e7)
+  results2[i] <- empprob(xdiff[xdiff > 0], -xdiff[xdiff < 0], 1e7)
 }
 names(results) <- nms
+names(results2) <- nms
+
+rbind(results, results2)
