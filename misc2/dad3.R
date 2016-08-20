25f1646c7f7fbd01b2beccc5514a31b026b08d35
Charles Zheng <snarles@stanford.edu>
2016-08-20 15:15:29 -0600

    amazon v

--- misc2/dad3.R
+++ misc2/dad3.R
@@ -1,21 +1,22 @@
 source("misc2/dad2.R")
 library(readxl)
-
+library(parallel)
 
 tab <- readxl::read_excel("misc2/CrossLorenz.xlsx")
 tab <- tab[1:18, -11]
 #lapply(tab, class)
-results <- numeric()
-results2 <- numeric()
-nms <- character()
-for (i in 1:9) {
-  nms[i] <- paste(tab[2 * i - 1, 1], tab[2 *i, 1], sep = "v")
-  xdiff <- as.numeric(tab[2 * i - 1, -1]) - as.numeric(tab[2 *i, -1])
-  xdiff <- xdiff/sd(xdiff)
-  results[i] <- empprob(xdiff[xdiff > 0], -xdiff[xdiff < 0], 1e7)
-  results2[i] <- empprob(xdiff[xdiff > 0], -xdiff[xdiff < 0], 1e7)
+getresults <- function(i) {
+  set.seed(i)
+  results <- numeric()
+  nms <- character()
+  for (i in 1:9) {
+    nms[i] <- paste(tab[2 * i - 1, 1], tab[2 *i, 1], sep = "v")
+    xdiff <- as.numeric(tab[2 * i - 1, -1]) - as.numeric(tab[2 *i, -1])
+    xdiff <- xdiff/sd(xdiff)
+    results[i] <- empprob(xdiff[xdiff > 0], -xdiff[xdiff < 0], 1e6)
+  }
+  names(results) <- nms
+  results  
 }
-names(results) <- nms
-names(results2) <- nms
 
-rbind(results, results2)
+allress <- mclapply(1:10, getresults, mc.cores = 3)
