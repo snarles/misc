de894ef7fad113903460a0b6118950bf60fcab55
Charles Zheng <snarles@stanford.edu>
2016-08-22 07:21:39 -0600

    check correlations

--- misc2/wangmin.R
+++ misc2/wangmin.R
@@ -2,3 +2,5 @@ dat <- read.csv("misc2/wmdata1.csv", header = FALSE)
 colnames(dat) <- c("outcome","cell_type","treatment")
 res <- lm(outcome ~ cell_type + treatment, data = dat)
 anova(res)
+cor(dat[dat[, 2]==0 & dat[, 3]==0, 1], dat[dat[, 2]==1 & dat[, 3]==0,1]) #  0.843771
+cor(dat[dat[, 2]==0 & dat[, 3]==1, 1], dat[dat[, 2]==1 & dat[, 3]==1,1]) #  0.2303471
