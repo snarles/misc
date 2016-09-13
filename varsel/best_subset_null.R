5c40b723e59322ef9f7392a8d50b849dd9153489
snarles <snarles@stanford.edu>
2016-09-12 18:44:42 -0700

    best subset null

---
+++ varsel/best_subset_null.R
@@ -0,0 +1,71 @@
+library(pracma)
+
+# compute_true_Vs <- function(X, mu, ss) {
+#   Xmu <- as.numeric(t(X) %*% mu)
+#   XtX <- t(X) %*% X
+#   ans <- apply(ss, 2, function(S) {
+#     S <- S[S != 0]
+#     as.numeric(t(Xmu[S]) %*% solve(XtX[S, S], Xmu[S]))
+#   })
+#   names(ans) <- apply(ss, 2, function(S) {
+#     S <- S[S != 0]
+#     paste(S, collapse = ".")
+#   })
+#   ans
+# }
+# 
+# compute_true_Vs2 <- function(X, mu, ss) {
+#   ans <- apply(ss, 2, function(S) {
+#     res <- qr(X[, S])
+#     qr.Q(res)
+#     sum((t(qr.Q(res)) %*% mu)^2)
+#   })
+#   names(ans) <- apply(ss, 2, function(S) {
+#     S <- S[S != 0]
+#     paste(S, collapse = ".")
+#   })
+#   ans
+# }
+
+## empirically compute true null distribution
+compute_true_Vs2_par <- function(X, mus, ss) {
+  ans <- apply(ss, 2, function(S) {
+    res <- qr(X[, S])
+    qr.Q(res)
+    colSums((t(qr.Q(res)) %*% mus)^2)
+  })
+  colnames(ans) <- apply(ss, 2, function(S) {
+    S <- S[S != 0]
+    paste(S, collapse = ".")
+  })
+  ans
+}
+
+normalize <- function(x) {
+  x/sqrt(sum(x^2))
+}
+
+n <- 30
+p <- 8
+X <- randn(n, p)
+X <- apply(X, 2, normalize)
+k <- 3
+ss <- combn(1:p, k)
+
+
+
+# y <- normalize(rnorm(n))
+# 
+# Vs <- compute_true_Vs(X, y, ss)
+# Vs2 <- compute_true_Vs2(X, y, ss)
+
+n.its <- 200
+ys <- randn(n, n.its)
+ys <- apply(ys, 2, normalize)
+# Vs <- compute_true_Vs2(X, ys[, 1], ss)
+Vss <- compute_true_Vs2_par(X, ys, ss)
+
+sup_dist <- apply(Vss, 1, max)
+hist(sup_dist)
+
+
