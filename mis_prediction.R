594796e2b2be36339de19895106bb43fc027ed27
snarles <snarles@stanford.edu>
2017-02-08 12:17:14 -0800

    might be upper bound

--- mis_prediction.R
+++ mis_prediction.R
@@ -1,7 +1,7 @@
 library(pracma)
-n <- 4
-p <- 2
-X <- randn(n, p) %*% randn(p)
+n <- 100
+p <- 20
+X <- randn(n, p) #%*% randn(p)
 x0 <- rnorm(p)
 
 h1 <- as.numeric(t(x0) %*% solve(t(X) %*% X, t(X)))
@@ -9,3 +9,5 @@ z <- as.numeric(X %*% x0)
 h2 <- sum(x0^2) * z/sum(z^2)
 plot(h1, h2)
 abline(0, 1)
+list(c(sum(abs(h1)), sum(abs(h2))),
+     c(sum(h1^2), sum(h2^2)))
